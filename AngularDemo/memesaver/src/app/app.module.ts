5bf42d2adbdfda6c72f171a6611ecd2facca280b
Mark Moore <mark.moore@revature.com>
2021-04-13 20:18:26 -0500

    adds project

---
+++ AngularDemo/memesaver/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { PersondetailsComponent } from './persondetails/persondetails.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ListpersonsComponent } from './listpersons/listpersons.component';
+import { ListimagesComponent } from './listimages/listimages.component';
+import { PersonjustdetailsComponent } from './personjustdetails/personjustdetails.component';
+import { MemejustdetailsComponent } from './memejustdetails/memejustdetails.component';
+import { DemoComponent } from './demo/demo.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    PersondetailsComponent,
+    ListpersonsComponent,
+    ListimagesComponent,
+    PersonjustdetailsComponent,
+    MemejustdetailsComponent,
+    DemoComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
